d0a14cc9122eb77e6753706568945f44ef41a1f3
subtlebutvivid <subtlebutvivid@gmail.com>
2018-06-27 02:14:12 +0000

    Update init file

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,20 @@ from config import app_config
 # db variable initialization
 db = SQLAlchemy()
 
+import os
+
 def create_app(config_name):
+
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
@@ -20,7 +33,6 @@ def create_app(config_name):
     Bootstrap(app)
 
     migrate = Migrate(app, db)
-    
 
     from app import models
 
